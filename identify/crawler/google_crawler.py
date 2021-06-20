f00162f7250d9a765f85f00b8a6e60b4abcfb493
buri-1029 <53345195+buri-1029@users.noreply.github.com>
2021-06-20 17:10:04 +0900

    Update google_crawler.py

--- identify/crawler/google_crawler.py
+++ identify/crawler/google_crawler.py
@@ -3,11 +3,19 @@ from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
-elem = driver.find_element_by_name("q")
-elem.send_keys("황금죽")
-elem.send_keys(Keys.RETURN)
+search_list = ["Acalypha reptans", "Adenium obesum", "Aeonium arboreum Zwartkop", "Alocasia amazonica", "Aloe barbadens", "Anthurium andraeanum", "Aspidistra elatior",
+"Begonia semperflorens", "Calathea crocata", "Calathea insignis", "Calathea makoyana", "Camellia japonica", "Ceropegia woodii", "Chamaedorea elegans", "Clivia miniata", "Clusia rosea",
+"Codiaeum variegatum", "Cupressus macrocarpa Wilma Goldcrest", "Cycas revoluta", "Cyclamen persicum", "Cymbidium spp", "Dendrobium phalaenopsis", "Dieffenbachia amoena Tropic snow",
+"Dracaena deremensis Virens Compacta", "Dracaena fragrans Massangeana", "Dracaena marginata", "Dracaena reflexa Song of India", "Dracaena sanderiana", "Dracaena sanderiana Virens",
+"Duranta reptans", "Epipremnum aureum", "Epipremnum aureum Lime", "Euonymus japonica", "Euphorbia pulcherrima", "Fatsia japonica", "Ficus elastica Tineke", "Ficus lyrata",
+"Fittonia verschaffelti White Star", "Gardenia jasminoides", "Guzmania lingulata", "Haemaria discolor dawsoniana", "Hedera helix", "Hemionitis arifolia", "Heteropanax fragrans", "Hoya carnosa"]
+
+for search in search_list:
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys(search)
+    elem.send_keys(Keys.RETURN)
 
 SCROLL_PAUSE_TIME = 1
 # Get scroll height
@@ -15,7 +23,8 @@ last_height = driver.execute_script("return document.body.scrollHeight")
 
 while True:
     # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);")
     # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
     # Calculate new scroll height and compare with last scroll height
@@ -28,6 +37,7 @@ while True:
     last_height = new_height
 
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+print(search)
 print(len(images))
 count = 1
 for image in images:
@@ -36,9 +46,10 @@ for image in images:
         time.sleep(2)
         imgUrl = driver.find_element_by_xpath(
             '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
-        path = './images/Dypsis lutescens/'
+        path = f"./{search}/"
         urllib.request.urlretrieve(imgUrl, path + str(count) + ".jpg")
         count = count + 1
     except:
         pass
+
 driver.close()
